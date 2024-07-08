cff4e401bf08d73989cb7ac53de8797f23071c33
paranoidxc <paranoid.xc@gmail.com>
2024-07-08 14:07:05 +0800

    fix noise

--- README.md
+++ README.md
@@ -6,6 +6,9 @@
 
 golang 练习的游戏
 
+- https://thebookofshaders.com/10/
+- shadertoy https://space.bilibili.com/10707223/channel/collectiondetail?sid=72563
+
 
 
 # 围观草班台子统治的世界一步一步崩塌
--- go/learn-with-game/noise/noise.go
+++ go/learn-with-game/noise/noise.go
@@ -2,6 +2,7 @@ package noise
 
 import (
 	"fmt"
+	"math"
 	"runtime"
 	"sync"
 )
@@ -52,8 +53,8 @@ func MakeNoise(noiseType NoiseType, frequency, lacunarity, gain float32, octaves
 
 	fmt.Println("freq:", frequency, "lac:", lacunarity, "gain:", gain, "octaves:", octaves)
 
-	min = float32(9999.0)
-	max = float32(-9999.0)
+	min = float32(math.MaxFloat32)
+	max = float32(-math.MaxFloat32)
 
 	numRoutines := runtime.NumCPU()
 	var wg sync.WaitGroup
@@ -63,6 +64,8 @@ func MakeNoise(noiseType NoiseType, frequency, lacunarity, gain float32, octaves
 	for i := 0; i < numRoutines; i++ {
 		go func(i int) {
 			defer wg.Done()
+			innerMin := float32(math.MaxFloat32)
+			innerMax := float32(-math.MaxFloat32)
 			start := i * batchSize
 			end := start + batchSize - 1
 			for j := start; j < end; j++ {
@@ -75,16 +78,21 @@ func MakeNoise(noiseType NoiseType, frequency, lacunarity, gain float32, octaves
 					noise[j] = Fbm2(float32(x), float32(y), frequency, lacunarity, gain, octaves)
 				}
 
-				if noise[j] < min || noise[j] > max {
-					mutex.Lock()
-					if noise[j] < min {
-						min = noise[j]
-					} else if noise[j] > max {
-						max = noise[j]
-					}
-					mutex.Unlock()
+				if noise[j] < innerMin {
+					innerMin = noise[j]
+				} else if noise[j] > innerMax {
+					innerMax = noise[j]
 				}
 			}
+
+			mutex.Lock()
+			if innerMin < min {
+				min = innerMin
+			}
+			if innerMax > max {
+				max = innerMax
+			}
+			mutex.Unlock()
 		}(i)
 	}
 	wg.Wait()
