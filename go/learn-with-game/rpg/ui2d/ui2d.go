2e71d810f88ecf9c3b77aa8b6b031fc49409b26f
xcHuang <paranoid.xc@gmail.com>
2024-07-26 23:15:10 +0800

    go rpg game

--- go/learn-with-game/rpg/ui2d/ui2d.go
+++ go/learn-with-game/rpg/ui2d/ui2d.go
@@ -183,6 +183,13 @@ func (ui *UI2d) Draw(level *game.Level) {
 				srcRects := textureIndex[tile]
 				srcRect := srcRects[rand.Intn(len(srcRects))]
 				dstRect := sdl.Rect{int32(x*32) + offsetX, int32(y*32) + offsetY, 32, 32}
+
+				pos := game.Pos{x, y}
+				if level.Debug[pos] {
+					textureAtlas.SetColorMod(128, 0, 0)
+				} else {
+					textureAtlas.SetColorMod(255, 255, 255)
+				}
 				renderer.Copy(textureAtlas, &srcRect, &dstRect)
 			}
 			//fmt.Println(tile)
@@ -227,6 +234,9 @@ func (ui *UI2d) GetInput() *game.Input {
 		if keyboardState[sdl.SCANCODE_RIGHT] == 0 && prevKeyboardState[sdl.SCANCODE_RIGHT] != 0 {
 			input.Typ = game.Right
 		}
+		if keyboardState[sdl.SCANCODE_S] == 0 && prevKeyboardState[sdl.SCANCODE_S] != 0 {
+			input.Typ = game.Search
+		}
 
 		for i, v := range keyboardState {
 			prevKeyboardState[i] = v
@@ -235,5 +245,7 @@ func (ui *UI2d) GetInput() *game.Input {
 		if input.Typ != game.None {
 			return &input
 		}
+
+		sdl.Delay(10)
 	}
 }
