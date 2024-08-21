8fa6bcfaab7572ac4b1cdd5a614f51296ae67804
xcHuang <paranoid.xc@gmail.com>
2024-08-21 21:45:52 +0800

    bird

--- Processing/examples/flappy_bird_ml_load/sketch.js
+++ Processing/examples/flappy_bird_ml_load/sketch.js
@@ -6,12 +6,20 @@ let timePass = 0;
 let reproductionCnt = 0;
 
 let gameState = 0;
+let gameStateInit = 0;
+let gameStateReady = 1;
+let gameStateRunning = 2;
+let gameStatePause = 3;
 let classifier;
+let myFont;
+
+function preload() {
+  myFont = loadFont("./font/flappy.TTF");
+}
 
 function setup() {
   createCanvas(640, 240);
   ml5.tf.setBackend("cpu");
-
   frameRate(60);
   classifier = ml5.neuralNetwork({
     inputs: 4,
@@ -29,70 +37,111 @@ function setup() {
 }
 
 function modelLoadedCallback() {
-  console.log("modelLoaded ready");
   for (let i = 0; i < 10; i++) {
     birds[i] = new Bird(classifier);
   }
   pipes.push(new Pipe());
-  gameState = 1;
+  gameState = gameStateReady;
 }
 
 function draw() {
   background(255);
-
-  if (gameState == 1) {
+  if (isGame([gameStateRunning, gameStateReady, gameStatePause])) {
     for (let i = pipes.length - 1; i >= 0; i--) {
-      pipes[i].update();
-      pipes[i].show();
-      if (pipes[i].offscreen()) {
-        pipes.splice(i, 1);
+      if (isGame([gameStateRunning])) {
+        pipes[i].update();
+      }
+
+      if (isGame([gameStateRunning, gameStatePause])) {
+        pipes[i].show();
+      }
+
+      if (isGame([gameStateRunning])) {
+        if (pipes[i].offscreen()) {
+          pipes.splice(i, 1);
+        }
       }
     }
 
     for (let bird of birds) {
       if (bird.alive) {
-        for (let pipe of pipes) {
-          if (pipe.collides(bird)) {
-            //noLoop();
-            bird.alive = false;
+        if (isGame([gameStateRunning])) {
+          for (let pipe of pipes) {
+            if (pipe.collides(bird)) {
+              //noLoop();
+              bird.alive = false;
+            }
           }
+          bird.think(pipes);
+          bird.update();
         }
-        bird.think(pipes);
-        bird.update();
-        bird.show();
+        if (isGame([gameStateRunning, gameStatePause])) {
+          bird.show();
+        }
       }
     }
 
-    if (frameCount % 100 == 0) {
-      pipes.push(new Pipe());
-    }
-
-    if (allBirdsDead()) {
-      normalizeFitness();
-      reproduction();
-      resetPipes();
-      if (timePass > maxTimePass) {
-        maxTimePass = timePass;
-        maxTimePassSec = floor(timePass / 60);
+    if (isGame([gameStateRunning])) {
+      if (frameCount % 100 == 0) {
+        pipes.push(new Pipe());
       }
-
-      timePass = 0;
-      reproductionCnt += 1;
-    } else {
-      timePass += 1;
     }
 
-    let timePassSec = floor(timePass / 60);
+    if (!isGame([gameStateReady])) {
+      if (allBirdsDead()) {
+        normalizeFitness();
+        reproduction();
+        resetPipes();
+        if (timePass > maxTimePass) {
+          maxTimePass = timePass;
+          maxTimePassSec = floor(timePass / 60);
+        }
 
+        timePass = 0;
+        reproductionCnt += 1;
+      } else {
+        timePass += 1;
+      }
+      let timePassSec = floor(timePass / 60);
+
+      push();
+      noFill();
+      stroke(0);
+      textSize(16);
+      textFont(myFont);
+      text(
+        `Round : ${reproductionCnt} Max : ${maxTimePassSec} Time : ${timePassSec}`,
+        width - 250,
+        20
+      );
+      pop();
+    }
+  }
+
+  if (isGame([gameStateReady])) {
     push();
-    fill(111);
-    noStroke();
-    textSize(16);
-    text(
-      `Round: ${reproductionCnt} MaxAlive: ${maxTimePassSec} Time: ${timePassSec}`,
-      width - 240,
-      20
-    );
+    textFont(myFont);
+    fill(255);
+    stroke(0);
+    strokeWeight(3);
+    textSize(50);
+    textAlign(CENTER, CENTER);
+    text("FLAPPY BIRD", width / 2, height / 4);
+    text("Machine Learning", width / 2, height / 2);
+    textSize(20);
+    text("press Space to Watch", width / 2, height / 1.2);
+    pop();
+  } else if (isGame([gameStatePause])) {
+    push();
+    textFont(myFont);
+    fill(255);
+    stroke(0);
+    strokeWeight(3);
+    textSize(50);
+    textAlign(CENTER, CENTER);
+    text("Pause", width / 2, height / 3);
+    textSize(20);
+    text("press P to Resume", width / 2, height / 1.6);
     pop();
   }
 }
@@ -141,4 +190,31 @@ function weightedSelection() {
 
 function resetPipes() {
   pipes.splice(0, pipes.length - 1);
+}
+
+function isGame(gameStates) {
+  for (let state of gameStates) {
+    if (state == gameState) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function keyPressed() {
+  console.log("keyPressed:", key);
+
+  if (isGame([gameStateReady])) {
+    if (key == " ") {
+      gameState = gameStateRunning;
+    }
+  } else if (isGame([gameStateRunning, gameStatePause])) {
+    if (key == "p") {
+      if (isGame([gameStateRunning])) {
+        gameState = gameStatePause;
+      } else {
+        gameState = gameStateRunning;
+      }
+    }
+  }
 }
