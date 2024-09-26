604d54f522e4ad81af044fbd65922c84b291ed9d
paranoidxc <paranoid.xc@gmail.com>
2024-09-26 10:58:13 +0800

    binlog

--- go/BinLogVisual/main.go
+++ go/BinLogVisual/main.go
@@ -7,10 +7,84 @@ import (
 	"strings"
 )
 
+const (
+	TIMESTAMP   = "SET TIMESTAMP="
+	TERMINAL    = "/*!*/;"
+	UPDATE      = "UPDATE"
+	INSERT      = "INSERT"
+	DELETE      = "DELETE"
+	HASH_UPDATE = "### UPDATE"
+	HASH_INSERT = "### INSERT"
+	HASH_DELETE = "### DELETE"
+)
+
+const (
+	IDX_TIMESTAMP   = 998
+	IDX_TERMINAL    = 999
+	IDX_UPDATE      = 1
+	IDX_INSERT      = 2
+	IDX_DELETE      = 3
+	IDX_HASH_UPDATE = 101
+	IDX_HASH_INSERT = 102
+	IDX_HASH_DELETE = 103
+)
+
+var mapCmdStringToIdx map[string]int
+
+func init() {
+	mapCmdStringToIdx = make(map[string]int)
+
+	mapCmdStringToIdx[TERMINAL] = IDX_TERMINAL
+
+	mapCmdStringToIdx[UPDATE] = IDX_UPDATE
+	mapCmdStringToIdx[HASH_UPDATE] = IDX_HASH_UPDATE
+	mapCmdStringToIdx[INSERT] = IDX_INSERT
+	mapCmdStringToIdx[HASH_INSERT] = IDX_HASH_INSERT
+	mapCmdStringToIdx[DELETE] = IDX_DELETE
+	mapCmdStringToIdx[HASH_DELETE] = IDX_HASH_DELETE
+}
+
 type Command struct {
+	Line      int
 	Typ       int
 	StartTime string
 	Sql       string
+}
+
+func getCommandType(s string) int {
+	r := 0
+	switch {
+	case strings.HasPrefix(s, UPDATE):
+		r = mapCmdStringToIdx[UPDATE]
+	case strings.HasPrefix(s, HASH_UPDATE):
+		r = mapCmdStringToIdx[HASH_UPDATE]
+	case strings.HasPrefix(s, INSERT):
+		r = mapCmdStringToIdx[INSERT]
+	case strings.HasPrefix(s, HASH_INSERT):
+		r = mapCmdStringToIdx[HASH_DELETE]
+	case strings.HasPrefix(s, DELETE):
+		r = mapCmdStringToIdx[DELETE]
+	case strings.HasPrefix(s, HASH_DELETE):
+		r = mapCmdStringToIdx[HASH_DELETE]
+	}
+
+	return r
+}
+
+func checkCommandTerminal(s string) int {
+	r := 0
+	switch {
+	case s == TERMINAL:
+		r = mapCmdStringToIdx[TERMINAL]
+	case strings.HasPrefix(s, HASH_UPDATE):
+		r = mapCmdStringToIdx[HASH_UPDATE]
+	case strings.HasPrefix(s, HASH_INSERT):
+		r = mapCmdStringToIdx[HASH_INSERT]
+	case strings.HasPrefix(s, HASH_DELETE):
+		r = mapCmdStringToIdx[HASH_DELETE]
+	}
+
+	return r
 }
 
 func main() {
@@ -23,64 +97,79 @@ func main() {
 	cmds := []Command{}
 
 	scanner := bufio.NewScanner(file)
-	idx := 0
 
 	isSqlFinish := true
 	isStartSql := false
 	startTime := ""
 
 	command := Command{}
+
+	line := 0
 	for scanner.Scan() {
-		if idx > 2000 {
+		line += 1
+
+		/*
+			if line > 100 {
+				break
+			}
+		*/
+		if len(cmds) > 50 {
 			break
 		}
 
 		s := scanner.Text()
 
+		/*
+			fmt.Println("line:", line)
+			fmt.Println("string:", s)
+		*/
+
+		prefix := ""
+		if len(s) >= 10 {
+			prefix = strings.ToUpper(s[:10])
+		}
+
 		if isStartSql && !isSqlFinish {
-			if s == "/*!*/;" {
-				//fmt.Printf("--------------------------------- \n cmmand %+v\n", command)
-				isStartSql = false
-				isSqlFinish = true
-				cmds = append(cmds, command)
-				command = Command{}
+			terminalType := checkCommandTerminal(s)
+			if terminalType > 0 {
+				if terminalType == IDX_TERMINAL {
+					isStartSql = false
+					isSqlFinish = true
+					cmds = append(cmds, command)
+					command = Command{Line: line}
+				} else {
+					cmds = append(cmds, command)
+					isStartSql = true
+					isSqlFinish = false
+					command = Command{Line: line, Typ: terminalType}
+					command.Sql = command.Sql + " " + strings.TrimSpace(s)
+				}
 			} else {
 				command.Sql = command.Sql + " " + strings.TrimSpace(s)
-			}
-			continue
-		}
-
-		if strings.HasPrefix(s, "SET TIMESTAMP=") {
-			startTime = s
-		} else if len(s) > 10 {
-			prefix := strings.ToUpper(s[:10])
-
-			if strings.HasPrefix(prefix, "INSERT") {
-				isStartSql = true
-			} else if strings.HasPrefix(prefix, "UPDATE") {
-				isStartSql = true
-			} else if strings.HasPrefix(prefix, "### UPDATE") || strings.HasPrefix(prefix, "### INSERT") {
-				cmds = append(cmds, command)
-				command = Command{}
-				isStartSql = true
-			} else if strings.HasPrefix(prefix, "DELETE") {
-				isStartSql = true
-			} else if strings.HasPrefix(prefix, "UPDATEx") {
-				//fmt.Println(s)
-				isStartSql = true
-			}
-
-			if isStartSql {
-				command.Sql = s
-				command.StartTime = startTime
-				isSqlFinish = false
+				continue
 			}
 		}
 
-		idx++
+		if !isStartSql {
+			if strings.HasPrefix(s, TIMESTAMP) {
+				startTime = strings.ReplaceAll(strings.ReplaceAll(s, TIMESTAMP, ""), TERMINAL, "")
+			} else if len(s) > 10 {
+				cmdType := getCommandType(prefix)
+				if cmdType > 0 {
+					isStartSql = true
+					command = Command{
+						Line:      line,
+						Typ:       cmdType,
+						Sql:       s,
+						StartTime: startTime,
+					}
+					isSqlFinish = false
+				}
+			}
+		}
 	}
 
 	for _, cmd := range cmds {
-		fmt.Printf("--------------------------------- \n cmmand %+v\n", cmd)
+		fmt.Printf("--------------------------------- \nCommand %+v\n", cmd)
 	}
 }
